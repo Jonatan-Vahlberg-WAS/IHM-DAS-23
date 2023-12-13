16006a1712ec8821acf6169d14b54ae6ab4afb83
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-12-13 10:17:13 +0100

    add string manipulations tasks

---
+++ string_manipulation.js
@@ -0,0 +1,16 @@
+
+
+//1. Make a fucntion that takes a string and returns it with all upper case letters
+//SV Skapa en funktion som tar en sträng och returnerar den med alla stora bokstäver
+
+//2. Make a function that takes a string and returns it with all lower case letters
+//SV Skapa en funktion som tar en sträng och returnerar den med alla små bokstäver
+
+//3. Take a string and split it into an array of words. Return that array.
+//SV Ta en sträng och dela upp den i en array av ord. Returnera den arrayen.
+
+//4. take a string and split it into sentences. Return that array.
+//SV Ta en sträng och dela upp den i meningar. Returnera den arrayen.
+
+//5. Take a string and find if it contains a specific word. Return true or false.
+//SV Ta en sträng och hitta om den innehåller ett specifikt ord. Returnera true eller false.
