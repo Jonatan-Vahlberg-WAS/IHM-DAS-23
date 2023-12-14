8f80dad056e42146c85631480c4649aa4be0a319
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-12-14 10:00:50 +0100

    add changes

---
+++ day_4_repition.js
@@ -0,0 +1,70 @@
+ // String manipulation
+ let text1 = "I love pizza" 
+        
+ /// Replace
+ text1 = text1.replace("pizza","🍕")
+ console.log(text1)
+
+ /// lower upper case & capitalize
+ console.log(text1.toLowerCase())
+ console.log(text1.toUpperCase())
+ console.log(text1[0].toUpperCase()+text1.substring(1))
+
+ let email = "TEST@test.com"
+ console.log(email.toLowerCase())
+
+ /// Split
+ let text2 = "The sun shines brightly in the morning sky. Birds chirp happily among the trees. A gentle breeze rustles the leaves.";
+
+ // Split into words 
+console.log(text2.split(" "))
+
+// Split into sentances
+const sentances = text2.split(".")
+const trimmedSentances = sentances.filter(s => s).map(s => s.trim())
+console.log(sentances)
+console.log(trimmedSentances)
+
+const filenames = [
+ "a_txt_file.txt",
+ "a broken txt file.txt"
+]
+
+// Solution 1
+const correctFileNames = filenames.map(fileName => fileName.replaceAll(" ", "_"))
+
+//solution 2 - Join
+
+const correctFileNames2 = filenames.map(fileName => {
+     const fileParts = fileName.split(" ")
+     const correctFileName = fileParts.join("_")
+     return correctFileName
+})
+
+console.log(filenames)
+console.log(correctFileNames)
+console.log(correctFileNames2)
+
+ // updating elements
+const title = document.getElementById("title")
+const description = document.getElementById("description")
+const navLinks = document.querySelectorAll("nav ul a") //Returns node list 
+ /// Styling
+ const pathname = document.location.pathname 
+navLinks.forEach(link => {
+ console.log(link.textContent)
+ link.textContent = link.textContent.toUpperCase()
+
+ const linkHref = link.getAttribute("href")
+ 
+ console.log(linkHref, pathname)
+ if(linkHref === pathname) {
+     link.classList.add("active")
+ }
+ if(linkHref === "/"){
+     link.textContent = "⌂"
+ }
+})
+
+
+ // Creating elements
